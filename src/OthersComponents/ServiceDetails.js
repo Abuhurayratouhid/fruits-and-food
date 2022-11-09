1d9d35e7f55f532a6fe1faa51fcfe4df2fa47cd1
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-11-09 10:54:33 +0600

    All details of service will displayed when user click details button

--- src/OthersComponents/ServiceDetails.js
+++ src/OthersComponents/ServiceDetails.js
@@ -3,10 +3,31 @@ import { useLoaderData } from 'react-router-dom';
 
 const ServiceDetails = () => {
     const service = useLoaderData()
-    const {name} = service;
+    const { name,picture, details ,_id,price, rating } = service;
     return (
         <div>
-            <h1>details page for : {name} </h1>
+            <h1 className='text-center my-10 text-4xl'>All details for : {name} </h1>
+            {/* service details section  */}
+            <div>
+                <div className="card lg:card-side bg-base-100 shadow-xl">
+                    <figure><img src={picture} alt="Album" /></figure>
+                    <div className="card-body">
+                        <h2 className="card-title">{name}</h2>
+                        <h2 className="card-title">Price:{price}</h2>
+                        <h2 className="card-title">Rating:{rating}</h2>
+                        <p>{details}</p>
+                        <div className="card-actions justify-end">
+                            {/* <button className="btn btn-primary">Listen</button> */}
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            {/* service review section  */}
+            <div>
+
+            </div>
         </div>
     );
 };
