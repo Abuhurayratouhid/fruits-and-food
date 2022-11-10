70239f843deaf775296208716af6ab3e972cdde6
Abuhurayratouhid <abuhurayratouhid@gmail.com>
2022-11-10 12:31:15 +0600

    user can add a service if he is loged in

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,14 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const AddService = () => {
+    const handleAddService = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const serviceName= form.name.value;
+        const servicePhoto = form.photoURL.value;
+        const servicePrice = form.price.value;
+        const serviceDetails = form.details.value;
+        console.log(serviceName, servicePhoto, servicePrice,serviceDetails)
+
+        const newService = {
+            name: serviceName,
+            photo: servicePhoto,
+            price : servicePrice,
+            details: serviceDetails
+        }
+
+        fetch('http://localhost:5000/addService',{
+            method: 'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(newService)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.insertedId){
+                toast.success('service added successfully')
+            }
+        })
+
+    }
     return (
         <div className=''>
             <h1 className='text-center my-10 text-4xl font-bold'>Add a service</h1>
             <div className="card flex-shrink-0 lg:w-3/4 lg:ml-40 md:h-[600px] shadow-2xl bg-gradient-to-r from-slate-300  to-slate-400">
-                    {/* signUp form  */}
+                    {/* add service  form  */}
 
-                    <form  className="card-body">
+                    <form onSubmit={handleAddService}  className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Service name: </span>
@@ -25,13 +59,13 @@ const AddService = () => {
                             <label className="label">
                                 <span className="label-text">Service price</span>
                             </label>
-                            <input type="text" name='email' placeholder="price" className="input input-bordered" required />
+                            <input type="text" name='price' placeholder="price" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Service details :</span>
                             </label>
-                            <input type="text" name='password' placeholder="details" className="input input-bordered" required/>
+                            <input type="text" name='details' placeholder="details" className="input input-bordered" required/>
                             
                             
                         </div>
@@ -41,6 +75,7 @@ const AddService = () => {
                         
                     </form>
                 </div>
+                <ToastContainer/>
         </div>
     );
 };
